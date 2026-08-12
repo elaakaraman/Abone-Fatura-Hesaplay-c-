1fa66d2ee4415a2622680e924c540db5b1f77292
elaakaraman <elanr.krmn00@gmail.com>
2026-08-12 09:25:27 +0300

    Add initial Program.cs with basic output

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("ABONE FATURA HESAPLAYICI");
+        Console.WriteLine("------------------------");
+        
+        Console.WriteLine("Proje başlatıldı.");
+    }
+}
