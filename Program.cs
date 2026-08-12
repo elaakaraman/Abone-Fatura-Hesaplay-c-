cf29f077587ff6b359e48606651946d276ba103f
elaakaraman <elanr.krmn00@gmail.com>
2026-08-12 09:26:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,41 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("ABONE FATURA HESAPLAYICI");
-        Console.WriteLine("------------------------");
-        
-        Console.WriteLine("Proje başlatıldı.");
+        Console.WriteLine("===== ABONE FATURA HESAPLAYICI =====");
+
+        Console.Write("Abone No: ");
+        string aboneNo = Console.ReadLine();
+
+        Console.Write("Abone Adı: ");
+        string aboneAdi = Console.ReadLine();
+
+        Console.Write("Kota (GB): ");
+        double kota = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Kullanılan İnternet (GB): ");
+        double kullanim = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Aylık Ücret (TL): ");
+        double aylikUcret = Convert.ToDouble(Console.ReadLine());
+
+        double asim = 0;
+        double asimUcreti = 0;
+
+        if (kullanim > kota)
+        {
+            asim = kullanim - kota;
+            asimUcreti = asim * 5;
+        }
+
+        double toplamFatura = aylikUcret + asimUcreti;
+
+        Console.WriteLine("\n===== FATURA BİLGİLERİ =====");
+        Console.WriteLine("Abone No: " + aboneNo);
+        Console.WriteLine("Abone Adı: " + aboneAdi);
+        Console.WriteLine("Kota: " + kota + " GB");
+        Console.WriteLine("Kullanım: " + kullanim + " GB");
+        Console.WriteLine("Kota Aşımı: " + asim + " GB");
+        Console.WriteLine("Aşım Ücreti: " + asimUcreti + " TL");
+        Console.WriteLine("Toplam Fatura: " + toplamFatura + " TL");
     }
 }
